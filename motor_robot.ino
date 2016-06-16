3f0e4426b141ba0c44d3fb1d228e01aa0f38bc32
Wayne Yeo <fishnsotong@gmail.com>
2016-06-16 16:37:52 +0800

    more fixes, unstable

--- motor_robot.ino
+++ motor_robot.ino
@@ -3,6 +3,8 @@ int NUM_KEYS = 5;
 int keyInput;
 int key =- 1;
 int oldkey =- 1;
+int pathArray[10] = {};
+int j = -1; // path counter
 
 void setup() {
   pinMode(13, OUTPUT); // This is an indicator of a button press.
@@ -10,6 +12,9 @@ void setup() {
 }
 
 void loop() {
+  if(sizeof(pathArray) == 40) {
+    Serial.println("TOO HIGH");
+  }
   keyInput = analogRead(A0);
   digitalWrite(13, LOW);
   //Serial.println(keyInput); // Prints the value, erase later, for debugging
@@ -22,25 +27,39 @@ void loop() {
     key = getKey(keyInput); //convert into keypress
     if (key != oldkey) {
       oldkey = key;
+      j++;
       if (key >= 0) {
         digitalWrite(13, HIGH);
         switch(key) {
-          case 0 : Serial.println("STOP BUTTON PRESSED");
+          case 0 : Serial.println("ENTER BUTTON PRESSED");
           break;
           case 1 : Serial.println("RESET BUTTON PRESSED");
           break;
           case 2 : Serial.println("LEFT BUTTON PRESSED");
+          pathArray[j] = 2;
           break;
           case 3 : Serial.println("FORWARD BUTTON PRESSED");
+          pathArray[j] = 1;
           break;
           case 4 : Serial.println("RIGHT BUTTON PRESSED");
+          pathArray[j] = 3;
           break;
+
+          Serial.println(pathArray[j]);
+
+
+          }
+
         }
       }
     }
-    delay(100);
+
+    for (j = j; j < 5; j++) {
+  Serial.println(pathArray[j]);
   }
+    delay(100);
 }
+
 
 // Converts keyInput value to key number
 int getKey(unsigned int input)
