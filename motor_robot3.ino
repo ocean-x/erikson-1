79da957f59e6fe5a428d6f6ee31a4d4db85eb584
Wayne Yeo <fishnsotong@gmail.com>
2016-06-19 15:06:41 +0800

    small fixes

--- motor_robot3.ino
+++ motor_robot3.ino
@@ -14,7 +14,7 @@ void setup() {
 }
 
 void loop() {
-  if(pathArray[7] != 0) {
+  if (pathArray[7] != 0) {
     Serial.println("You've ran out of spaces!"); //player has ran out of possible moves
   }
   digitalWrite(13, LOW);
@@ -30,23 +30,38 @@ void loop() {
     if (key != oldkey) {
       oldkey = key;
       if (key >= 0) {
+        tone(10, 4000, 200);
         digitalWrite(13, HIGH);
         switch(key) {
           case 0 : Serial.println("ENTER BUTTON PRESSED");
+                   pass = check();
+                   tone(10, 4000, 200);
           break;
           case 1 : Serial.println("RESET BUTTON PRESSED");
+                   int k;
+                   for (k = 0; k < 8; k++) {
+                     pathArray[k] = 0;
+                   }
+                   j = 0;
+                   tone(10, 4000, 200);
           break;
           case 2 : Serial.println("LEFT BUTTON PRESSED");
-          pathArray[j] = 2;
+                   pathArray[j] = 2;
+                   j++;
+                   tone(10, 4000, 200);
           break;
           case 3 : Serial.println("FORWARD BUTTON PRESSED");
-          pathArray[j] = 1;
+                   pathArray[j] = 1;
+                   j++;
+                   tone(10, 4000, 200);
           break;
           case 4 : Serial.println("RIGHT BUTTON PRESSED");
-          pathArray[j] = 3;
+                   pathArray[j] = 3;
+                   j++;
+                   tone(10, 4000, 200);
           break;
           }
-          j++;
+
         }
       }
     }
@@ -68,3 +83,15 @@ int getKey(unsigned int input)
          if (k >= NUM_KEYS)k = -1;  // No valid key pressed
          return k;
   }
+
+bool check() {
+  int k;
+  for (k = 0; k <8; k++) {
+    if (targetArray[k] != pathArray[k]) {
+      Serial.println("You've got it wrong, try again?");
+      return false;
+    }
+  }
+  Serial.println("Yay! Congratulations, the clue to your next puzzle is: ");
+  return true;
+}
