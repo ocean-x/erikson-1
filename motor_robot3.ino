13a99be9bbb28509e6c77407646046464f3e2c02
Wayne Yeo <fishnsotong@gmail.com>
2016-06-19 18:14:29 +0800

    more fixes

--- motor_robot3.ino
+++ motor_robot3.ino
@@ -1,8 +1,10 @@
 #include <Wire.h>
-#include <Servo.h>
-#define BACKLIGHT_PIN     3
+#include <LiquidCrystal_I2C.h>
+#define BACKLIGHT_PIN     13
 #define ROTATION_SPEED 150
 #define DEFAULT_SPEED 150
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+
 int keyValue[5] = {20, 55, 100, 200, 600}; //actual values are closer, but rounded to nearest 10.
 int NUM_KEYS = 5;
 int keyInput;
@@ -17,27 +19,19 @@ const int MOTOR_LEFT_IN3 = 4;
 const int MOTOR_LEFT_IN4 = 5;
 bool pass = false; // initial pass condition is false.
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, BACKLIGHT_PIN, POSITIVE);  // Set the LCD I2C address
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
+void setup() {
+  // Switch on the backlight
   pinMode ( BACKLIGHT_PIN, OUTPUT );
   digitalWrite ( BACKLIGHT_PIN, HIGH );
+
   lcd.begin(16,2);
-  lcd.home();
+  lcd.home ();
   lcd.print("Hello, ARDUINO ");
-  lcd.setCursor( 0, 1 );
+  lcd.setCursor (0,1);
   lcd.print ("This is an LCD");
-  robotStop();
-  delay(500);
-  robotBack();
-  delay(2000);
-  robotStop();
-  //int angle = sweepForBestAngle();
-  //angle = angle - 90;
-  robotRotateLeft(90);
-  robotForward();
+
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
@@ -47,9 +41,9 @@ void loop() {
   digitalWrite(13, LOW);
   keyInput = analogRead(0);
   //Serial.println(keyInput); // Prints the value, erase later, for debugging
-​
+
   key = getKey(keyInput); // record keypress
-​
+
   if (key != oldkey) {     //if keypress is detected
     delay(50);
     keyInput = analogRead(0);
@@ -63,7 +57,6 @@ void loop() {
           case 0 : Serial.println("ENTER BUTTON PRESSED");
                    pass = check();
                    tone(10, 4000, 200);
-                   if(pass==true) move();
           break;
           case 1 : Serial.println("RESET BUTTON PRESSED");
                    int k;
@@ -89,27 +82,62 @@ void loop() {
                    tone(10, 4000, 200);
           break;
           }
-​
+
         }
       }
     }
   }
-}​
+
 void move() {
-  for(int i=0;i<targetArray.length;i++) {
+  for(int i=0;i<8;i++) {
     if(targetArray[i] == 1) robotForward();
     else if(targetArray[i] == 2) robotRotateLeft();
     else if(targetArray[i] == 3) robotRotateRight();
   }
 }
-​
 
+
+// Converts keyInput value to key number
+int getKey(unsigned int input)
+  {
+      int k;
+      for (k = 0; k < NUM_KEYS; k++)
+      {
+        if (input < keyValue[k])
+       {
+              return k;
+          }
+     }
+         if (k >= NUM_KEYS)k = -1;  // No valid key pressed
+         return k;
+  }
+
+bool check() {
+  int k;
+  for (k = 0; k <8; k++) {
+    if (targetArray[k] != pathArray[k]) {
+      Serial.println("You've got it wrong, try again?");
+      return false;
+    }
+  }
+  Serial.println("Yay! Congratulations, the clue to your next puzzle is: ");
+  return true;
+}
+
+/*
+1. code LCD (everything is still in serial)
+2. drive the motors (because this is still a screen puzzle, nothing moves)
+3. build the fucking robot (but athena is overseas)
+*/
 
 /**************************************************/
 // Don't change the code below unless
 // you know what you're doing :)
+// Angle sweep is when you have the ultrasonic sensor and servo
 /**************************************************/
-int sweepForBestAngle() {
+
+
+/*int sweepForBestAngle() {
   int pos;
   int bestDistance = 0;
   int bestAngle = 0;
@@ -117,6 +145,7 @@ int sweepForBestAngle() {
   // Move to starting position
   distanceServo.write(0);
   delay(240);
+
   for(pos = 0; pos <= 180; pos += 30) {
     distanceServo.write(pos);
     delay(300);
@@ -132,13 +161,15 @@ int sweepForBestAngle() {
 
   distanceServo.write(bestAngle);
   delay(240);
-}
+}*/
+
 void robotRotateLeft() {
   digitalWrite(MOTOR_RIGHT_IN1, HIGH);
   analogWrite(MOTOR_RIGHT_IN2, 255 - ROTATION_SPEED);
   digitalWrite(MOTOR_LEFT_IN3, LOW);
   analogWrite(MOTOR_LEFT_IN4, ROTATION_SPEED);
 }
+
 void robotRotateLeft(int angle) {
   if (angle < 0) {
     robotRotateRight();
@@ -149,12 +180,14 @@ void robotRotateLeft(int angle) {
   delay(angle*4);
   robotStop();
 }
+
 void robotRotateRight() {
   digitalWrite(MOTOR_RIGHT_IN1, LOW);
   analogWrite(MOTOR_RIGHT_IN2, ROTATION_SPEED);
   digitalWrite(MOTOR_LEFT_IN3, HIGH);
   analogWrite(MOTOR_LEFT_IN4, 255 - ROTATION_SPEED);
 }
+
 void robotRotateRight(int angle) {
   if (angle < 0) {
     robotRotateRight();
@@ -165,12 +198,15 @@ void robotRotateRight(int angle) {
   delay(angle*4);
   robotStop();
 }
+
 void robotForward() {
   robotForward(DEFAULT_SPEED);
 }
+
 void robotBack() {
   robotForward(-DEFAULT_SPEED);
 }
+
 void robotForward(int speed) {
   speed = constrain(speed, -255, 255);
   if (speed == 0) {
@@ -189,6 +225,7 @@ void robotForward(int speed) {
     analogWrite(MOTOR_LEFT_IN4, -speed);
   }
 }
+
 void robotForward(long duration, int speed) {
   robotForward(speed);
   delay(duration);
@@ -196,38 +233,9 @@ void robotForward(long duration, int speed) {
 }
 
 void robotStop() {
+  // Stop
   digitalWrite(MOTOR_RIGHT_IN1, LOW);
   analogWrite(MOTOR_RIGHT_IN2, LOW);
   digitalWrite(MOTOR_LEFT_IN3, LOW);
   analogWrite(MOTOR_LEFT_IN4, LOW);
 }
-// Converts keyInput value to key number
-int getKey(unsigned int input)
-  {
-      int k;
-      for (k = 0; k < NUM_KEYS; k++)
-      {
-        if (input < keyValue[k])
-       {
-              return k;
-          }
-     }
-         if (k >= NUM_KEYS)k = -1;  // No valid key pressed
-         return k;
-  }
-bool check() {
-  int k;
-  for (k = 0; k <8; k++) {
-    if (targetArray[k] != pathArray[k]) {
-      Serial.println("You've got it wrong, try again?");
-      return false;
-    }
-  }
-  Serial.println("Yay! Congratulations, the clue to your next puzzle is: ");
-  return true;
-}
-/*
-1. code LCD (everything is still in serial)
-2. drive the motors (because this is still a screen puzzle, nothing moves)
-3. build the fucking robot (but athena is overseas)
-*/
