562b5881bcf69a340328b5de26a133b19cf2464c
Wayne Yeo <fishnsotong@gmail.com>
2016-07-09 19:25:02 +0800

    pin assignments and accelerometer initialisation

--- erikson-1/erikson-1.ino
+++ erikson-1/erikson-1.ino
@@ -1,12 +1,49 @@
+/*
+OCEANX CONTROL SYSTEM
+Tested with Arduino Mega 2560
+
+X Axis pointing forward
+Y Axis pointing to the right
+Z Axis pointing down
+Positive pitch: nose up
+Positive roll: right wing down
+Positive yaw: clockwise
+*/
+
+//libraries
+#include <Wire.h>
+#include <Servo.h>
+#include "NAxisMotion"
+
+//constants, might need to find a more elegant solution using arrays
+Servo ESC_LEFT_HORIZ;
+Servo ESC_RIGHT_HORIZ;
+Servo ESC_LEFT_FRONT_VERT;
+Servo ESC_LEFT_BACK_VERT;
+Servo ESC_RIGHT_FRONT_VERT;
+Servo ESC_RIGHT_BACK_VERT;
+
+//pin assignments: motors, lights, sensors
+const int ESC_LEFT_HORIZ = 5;
+const int ESC_LEFT_FRONT_VERT = 6;
+const int ESC_LEFT_BACK_VERT = 7;
+
+const int ESC_RIGHT_HORIZ = 8;
+const int ESC_RIGHT_FRONT_VERT = 9;
+const int ESC_RIGHT_BACK_VERT = 10;
+
+const int lightPin = 3;
+const int tempPin = 4;
+const int pHPin = 5;
+boolean lightEnable = false;
+
 void setup() {
-	pinMode(13, OUTPUT);
+	Serial.begin(112500);
+	I2C.begin();
+	pinMode(lightPin, OUTPUT);
+
 }
 
 void loop() {
-	digitalWrite(13, HIGH);
-	delay(100);
-	digitalWrite(13, LOW);
-	delay(100);
-}
 
-// I know this is blink, but yes, Git cannot detect empty files and I have nothing better to type at this moment.
+}
--- motion.ino
+++ motion.ino
@@ -2,11 +2,37 @@
 #include <Servo.h>
 #include "NAxisMotion.h" // Bridge code between API and Arduino Environment
 
+NAxisMotion ROVSensor;
+unsigned long lastStreamTime = 0; //store last steamed time stamp
+const int streamPeriod = 40; //stream at 25Hz. period time is inversely proportional to frequency (1000/40)
+bool upDateSensorData = true;
+
 void setup() {
   Serial.begin(112500);
   I2C.begin();
   mySensor.initSensor();
-  mySensor.setOperationMode(OPERATION_MODE_NDOF);
+  mySensor.setOperationMode(0x0C); // 9 Degrees of Freedom Sensor Fusion, can be changed to debug
+  mySensor.setUpdateMode(MANUAL);
+  // TODO: below is the configuration of the accelerometer, now need to add the other 2 sensors. 
+  mySensor.updateAccelConfig();
+  updateSensorData = true;
+  Serial.println();
+  Serial.println("Default accelerometer configuration settings...");
+  Serial.print("Range: ");
+  Serial.println(mySensor.readAccelRange());
+  Serial.print("Bandwidth: ");
+  Serial.println(mySensor.readAccelBandwidth());
+  Serial.print("Power Mode: ");
+  Serial.println(mySensor.readAccelPowerMode());
+  Serial.println("Streaming in ...");	//Countdown
+  Serial.print("3...");
+  delay(1000);	//Wait for a second
+  Serial.print("2...");
+  delay(1000);	//Wait for a second
+  Serial.println("1...");
+  delay(1000);	//Wait for a second
+}
+
 
 }
 
