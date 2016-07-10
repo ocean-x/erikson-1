027a0b58bca98077d09753a78bcc0c12e78ae460
Wayne Yeo <fishnsotong@gmail.com>
2016-07-10 13:39:16 +0800

    Fixed syntactic bugs

--- motion.ino
+++ motion.ino
@@ -5,7 +5,7 @@
 NAxisMotion ROVSensor;
 unsigned long lastStreamTime = 0; //store last steamed time stamp
 const int streamPeriod = 40; //stream at 25Hz. period time is inversely proportional to frequency (1000/40)
-bool upDateSensorData = true;
+bool updateSensorData = true;
 
 Servo esc;
 const int control = 3;
@@ -23,11 +23,11 @@ void setup() {
   Serial.println();
   Serial.println("Default accelerometer configuration settings...");
   Serial.print("Range: ");
-  Serial.println(mySensor.readAccelRange());
+  Serial.println(ROVSensor.readAccelRange());
   Serial.print("Bandwidth: ");
-  Serial.println(mySensor.readAccelBandwidth());
+  Serial.println(ROVSensor.readAccelBandwidth());
   Serial.print("Power Mode: ");
-  Serial.println(mySensor.readAccelPowerMode());
+  Serial.println(ROVSensor.readAccelPowerMode());
   Serial.println("Streaming in ...");	//Countdown
   Serial.print("3...");
   delay(1000);	//Wait for a second
@@ -40,17 +40,17 @@ void setup() {
 }
 
 
-}
+
 
 void loop() {
   // TODO: add magnetic data and find a way to meaningfully display on the RPi
 
   if (updateSensorData)  //Keep the updating of data as a separate task
   {
-    mySensor.updateAccel();        //Update the Accelerometer data
-    mySensor.updateLinearAccel();  //Update the Linear Acceleration data
-    mySensor.updateGravAccel();    //Update the Gravity Acceleration data
-    mySensor.updateCalibStatus();  //Update the Calibration Status
+    ROVSensor.updateAccel();        //Update the Accelerometer data
+    ROVSensor.updateLinearAccel();  //Update the Linear Acceleration data
+    ROVSensor.updateGravAccel();    //Update the Gravity Acceleration data
+    ROVSensor.updateCalibStatus();  //Update the Calibration Status
     updateSensorData = false;
   }
 
@@ -63,43 +63,43 @@ void loop() {
     Serial.print("ms ");
 
     Serial.print("      aX: ");
-    Serial.print(mySensor.readAccelX()); //Accelerometer X-Axis data
+    Serial.print(ROVSensor.readAccelX()); //Accelerometer X-Axis data
     Serial.print("m/s2 ");
 
     Serial.print(" aY: ");
-    Serial.print(mySensor.readAccelY());  //Accelerometer Y-Axis data
+    Serial.print(ROVSensor.readAccelY());  //Accelerometer Y-Axis data
     Serial.print("m/s2 ");
 
     Serial.print(" aZ: ");
-    Serial.print(mySensor.readAccelZ());  //Accelerometer Z-Axis data
+    Serial.print(ROVSensor.readAccelZ());  //Accelerometer Z-Axis data
     Serial.print("m/s2 ");
 
     Serial.print("      lX: ");
-    Serial.print(mySensor.readLinearAccelX()); //Linear Acceleration X-Axis data
+    Serial.print(ROVSensor.readLinearAccelX()); //Linear Acceleration X-Axis data
     Serial.print("m/s2 ");
 
     Serial.print(" lY: ");
-    Serial.print(mySensor.readLinearAccelY());  //Linear Acceleration Y-Axis data
+    Serial.print(ROVSensor.readLinearAccelY());  //Linear Acceleration Y-Axis data
     Serial.print("m/s2 ");
 
     Serial.print(" lZ: ");
-    Serial.print(mySensor.readLinearAccelZ());  //Linear Acceleration Z-Axis data
+    Serial.print(ROVSensor.readLinearAccelZ());  //Linear Acceleration Z-Axis data
     Serial.print("m/s2 ");
 
     Serial.print("      gX: ");
-    Serial.print(mySensor.readGravAccelX()); //Gravity Acceleration X-Axis data
+    Serial.print(ROVSensor.readGravAccelX()); //Gravity Acceleration X-Axis data
     Serial.print("m/s2 ");
 
     Serial.print(" gY: ");
-    Serial.print(mySensor.readGravAccelY());  //Gravity Acceleration Y-Axis data
+    Serial.print(ROVSensor.readGravAccelY());  //Gravity Acceleration Y-Axis data
     Serial.print("m/s2 ");
 
     Serial.print(" gZ: ");
-    Serial.print(mySensor.readGravAccelZ());  //Gravity Acceleration Z-Axis data
+    Serial.print(ROVSensor.readGravAccelZ());  //Gravity Acceleration Z-Axis data
     Serial.print("m/s2 ");
 
     Serial.print("      C: ");
-    Serial.print(mySensor.readAccelCalibStatus());  //Accelerometer Calibration Status (0 - 3)
+    Serial.print(ROVSensor.readAccelCalibStatus());  //Accelerometer Calibration Status (0 - 3)
 
     Serial.println();
 
