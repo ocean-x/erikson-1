c08c256c0bdaf2b51c2b6ee1cc8baf500296a91f
Wayne Yeo <fishnsotong@gmail.com>
2016-07-18 01:48:51 +0800

    finished writing calibration file

--- motor_calib.ino
+++ motor_calib.ino
@@ -2,6 +2,8 @@
 OCEANX MOTOR CALIBRATION SYSTEM –
 Tested with Arduino Mega 2560
 
+Turn on the serial monitor for this to work
+
 X Axis pointing forward
 Y Axis pointing to the right
 Z Axis pointing down
@@ -21,15 +23,29 @@ TODO: Program motion functions
 #include <Servo.h>
 
 #define MAX_SIGNAL 2000
-#define MIN_SIGNAL 700
+#define MIN_SIGNAL 1000
 #define MOTOR_PIN 9
 
 Servo motor;
 
 void setup() {
+  Serial.begin(9600);
+	Serial.println("This program will calibrate the ESC");
 
+	motor.attach(MOTOR_PIN);
+
+	Serial.println("Now writing maximum output");
+	Serial.println("Turn on power source, wait for 2 seconds and press any key");
+	motor.writeMicroseconds(MAX_SIGNAL);
+
+	while (!Serial.available()) {
+		Serial.read();
+	}
+
+	Serial.println("Sending minimum output");
+	motor.writeMicroseconds(MIN_SIGNAL);
 }
 
 void loop() {
-  
+
 }
