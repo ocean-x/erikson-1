e2ff092d2d190555175ab6d35a375f9bf3bade5a
Wayne Yeo <fishnsotong@gmail.com>
2016-07-18 11:50:30 +0800

    compass heading

---
+++ erikson-1/Compass.ino
@@ -0,0 +1,13 @@
+void compassHeading() {
+  float MAG_X;
+  float MAG_Y;
+  float cos_roll;
+  float sin_roll;
+  float cos_pitch;
+  float sin_pitch;
+
+  cos_roll = cos(roll);
+  sin_roll = sin(roll);
+  cos_pitch = cos(pitch);
+  sin_pitch = sin(pitch);
+}
